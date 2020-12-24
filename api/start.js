c2685a0e5806992e515c65dc875e5885efca3303
BipRaider <thebipus@gmail.com>
2020-12-24 20:46:16 +0200

    load check

--- api/start.js
+++ api/start.js
@@ -1,3 +1,7 @@
-const SlimMomServer = require("./server");
+const SlimMomServer = require('./server');
 
 new SlimMomServer().start();
+
+// npm i --save module-alias
+
+//https://www.npmjs.com/package/module-alias
