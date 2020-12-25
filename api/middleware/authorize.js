6d23052cf1b1bd53c24f313638fd7069dead8d18
BipRaider <thebipus@gmail.com>
2020-12-25 18:30:40 +0200

    Added aut for user

--- api/middleware/authorize.js
+++ api/middleware/authorize.js
@@ -7,12 +7,8 @@ const { RequestError } = require('../helpers');
 
 async function authorize(req, res, next) {
    try {
-      // 1. достать токен пользователя с заголовка Authorization
       const authorizationHeader = req.get('Authorization' || '');
-      const token = authorizationHeader.replace('Bearer ', ''); // убрали слово Bearer  и получили чистый токен
-
-      // 2. достать id пользователя с пейлоада или вернуть пользователю
-      // ошибку со статус кодом 401
+      const token = authorizationHeader.replace('Bearer ', '');
       let userId;
       try {
          userId = await jwt.verify(token, JWT_SECRET).id;
@@ -20,17 +16,13 @@ async function authorize(req, res, next) {
          next(new RequestError('User not authorized', 401));
          // throw new RequestError('User not authorized', 401);
       }
-      // 3. достать соответствующего пользователя. Если такого нет - вернуть
-      // ошибку со статус кодом 401
-      // userModel - модель пользователя в нашей системе
+
       const user = await userModule.findById(userId);
       console.dir(user);
       if (!user || user.accessToken !== token) {
          throw new RequestError('User not authorized', 401);
       }
 
-      // 4. Если все прошло успешно - передать запись пользователя и токен в req
-      // и передать обработку запроса на следующий middleware
       req.user = user;
       req.accessToken = token;
       req.refreshToken = user.refreshToken;
