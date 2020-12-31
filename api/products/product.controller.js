3955276e175ef6f6810d1a7596ce1495e5247302
SergiiPodlishchuk <podluy23@gmail.com>
2020-12-31 10:14:15 +0200

    swager json

--- api/products/product.controller.js
+++ api/products/product.controller.js
@@ -10,9 +10,9 @@ module.exports = class ProductController {
    static async getProducts(req, res, next) {
       try {
          const { search } = req.query;
-
+         console.log(req.query);
          const products = await ProductService.getProducts(search);
-         console.log(products);
+         // console.log(products);
 
          return res.status(200).json(products);
       } catch (error) {
