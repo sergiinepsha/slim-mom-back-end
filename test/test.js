69bfe2fbe9ffbf34de550886d1bdf94c8495cc6a
LeanNSP <nsp.lean@gmail.com>
2021-01-04 19:26:47 +0200

    adds product.router.acceptance.spec

--- test/test.js
+++ test/test.js
@@ -4,11 +4,14 @@ const sinon = require('sinon');
 const should = require('should');
 
 //--------test is ok------------
+//---------unit tests-----------
 require('./helpers/calculateDaySummary.unit.spec');
 require('./helpers/calculateDailyRate.unit.spec');
 
-//------------------------------
+//-------acceptance tests-------
+require('./router/product.router.acceptance.spec');
+//--//--//--//--//--//--//--//--
 
 //------test in progress--------
-// require('./router/product.router.acceptance.spec');
+
 //------------------------------
