a69daee29512b4f3b9c4c7a6f70e63856325b9ce
BipRaider <thebipus@gmail.com>
2021-01-04 17:10:06 +0200

    Added test for gerEmail func

--- test/auth/getEmail.test.js
+++ test/auth/getEmail.test.js
@@ -6,21 +6,34 @@ const should = require('should');
 const { getEmail } = require('../../api/helpers');
 const connectionOnDB = require('../../api/connectionOnDB');
 
-describe('getEmail', () => {
+describe('Function helpers/getEmail()', () => {
+   let emailTest = 'bip@bip.com';
    let testEmail;
 
    beforeEach(async () => {
       await connectionOnDB();
-      testEmail = await getEmail('bip@bip.com');
+      testEmail = await getEmail(emailTest);
    });
 
-   after(() => {
+   afterEach(() => {
       mongoose.disconnect();
    });
 
-   it('Get user by email from database', () => {
-      testEmail.should.be.containDeep({ email: 'bip@bip.com' });
+   it('Get user by email from database whit corrected answered ', () => {
+      // testEmail.should.be.containDeep({ email: 'bip@bip.com' });
+      testEmail.should.be.ok();
+      testEmail.should.be.have.property('name').which.is.a.String();
+      testEmail.should.be.have.property('email').which.is.a.String();
+      testEmail.should.be.have.property('password').which.is.a.String();
+      testEmail.should.be.have.property('accessToken').which.is.a.String();
+      testEmail.should.be.have.property('refreshToken').which.is.a.String();
+      testEmail.should.be.have.property('sid').which.is.a.String();
+      testEmail.should.be.have.property('days').which.is.a.Array();
    });
-
-   it('Return throw', () => {});
+   it('When no email  it throw error 401', async () => {
+      return await await getEmail('s').should.be.rejectedWith(Error, {
+         code: 401,
+         message: 'Email or password is wrong',
+      });
+   });
 });
