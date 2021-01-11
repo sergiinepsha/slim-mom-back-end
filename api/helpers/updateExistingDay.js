038a8b58961ea839e46ce1bae4ae49a53fed0470
LeanNSP <nsp.lean@gmail.com>
2021-01-11 22:30:57 +0200

    adds notAllowedProducts

--- api/helpers/updateExistingDay.js
+++ api/helpers/updateExistingDay.js
@@ -4,10 +4,22 @@ const dayModel = require('../models/day.model');
 
 const calculateEatenProduct = require('./calculateEatenProduct');
 const updateDaySummary = require('./updateDaySummary');
+const isNotAllowedProduct = require('./isNotAllowedProduct');
+const isFirstNotAllowedProduct = require('./isFirstNotAllowedProduct');
 
-module.exports = async (eatenProduct, weight, dayId, dailyRate) => {
+module.exports = async (currentUser, eatenProduct, weight, dayId) => {
+   const { dailyRate, bloodType } = currentUser.userData;
+
    try {
       const productCalculated = calculateEatenProduct(eatenProduct, weight);
+
+      const notAllowed = isNotAllowedProduct(bloodType, eatenProduct);
+
+      if (notAllowed) {
+         const notAllowedProduct = await isFirstNotAllowedProduct(dayId, notAllowed);
+
+         await dayModel.findDayByIdAndUpdateNotAllowedProducts(dayId, notAllowedProduct);
+      }
 
       const updatedDayEatenProducts = await dayModel.findDayByIdAndUpdateEatenProducts(
          dayId,
